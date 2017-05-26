443adcb64b6a1af49a1518b9d2bc9c3d50b3b764
houchuanhao <houchuanhao@imudges.com>
2017-05-26 09:02:16 -0700

    看到了5.3.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'foo/bar'
+  	get 'foo/bar'
 
-  get 'foo/baz'
+  	get 'foo/baz'
 
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help'
-  get 'static_pages/contact'
-  root 'static_pages#home'
+	root 'static_pages#home'
+	get '/help', to: 'static_pages#help'#,as: 'helf'
+	get '/about', to: 'static_pages#about'
+	get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
