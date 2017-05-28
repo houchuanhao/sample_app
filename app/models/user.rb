5b2d50b6843c9ac7d3fa897cfbf1590d3c79402a
houchuanhao <houchuanhao@imudges.com>
2017-05-27 20:34:59 -0700

    完成了电子邮件的长度验证

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+	validates(:name,presence: true,length: {maximum:50}) #两个参数，一个是name,一个是哈希参数：{presence: true}
+	validates(:email,presence: true,length:{maximum: 250})
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,26 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+  	@user=User.new(name:"岳丽珠",email:"ylz@hch.com")
+  end
+  test "should be vaild" do
+  	assert	@user.valid?
+  end
+  test "name should be present" do
+	@user.name = "    "
+	assert_not @user.valid?
+  end
+  test "email should also be present" do
+  	@user.email=""
+  	assert_not @user.valid?
+  end
+  test "用户名长度不超过51" do
+  	@user.name="a"*51
+  	assert_not @user.valid?
+  end
+  test "邮箱不能超过244" do
+  	@user.email="a"*244+"@imudges.com"
+  	assert_not @user.valid?
+  end
 end
