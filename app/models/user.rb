a8a5505f41f19ad4739d4a11c1b9d5d625316ac3
houchuanhao <houchuanhao@imudges.com>
2017-05-28 01:38:37 -0700

    存入数据库之气都会做成小写

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	                   #/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+	before_save {self.email=email.downcase}
 	validates(:name, 
 		presence: true,
 		length: {maximum:50}
