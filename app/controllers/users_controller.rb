4aea7d970546ebf419e068116c842b709992f248
houchuanhao <houchuanhao@imudges.com>
2017-05-28 20:25:29 -0700

    7.4.3中rails db:migrate:reset执行成功！！

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,17 @@ class UsersController < ApplicationController
   	@user=User.find(params[:id])
   end
   def create
-@user = User.new(user_params)
-if @user.save
-# 处理注册成功的情况
-else
-render 'new'
-end
-end
+	@user = User.new(user_params)
+	if @user.save
+		flash[:success] = "Welcome to the Sample App!"
+		redirect_to @user 
+		#或者 redirect_to user_url(@user)
+	else
+	 render 'new'
+	end
+  end
 private
-def user_params
-params.require(:user).permit(:name, :email, :password,
-:password_confirmation)
-end
+  def user_params
+	params.require(:user).permit(:name, :email, :password,:password_confirmation)
+  end
 end
