5990e4c3fe56bf806130719ba5d5776049e120ca
houchuanhao <houchuanhao@imudges.com>
2017-05-30 07:35:58 -0700

    可以运行

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,9 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			log_in user
-			params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-			redirect_back_or user
+			#params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+			#redirect_back_or user
+			redirect_to user
 			# 登入用户,然后重定向到用户的资料页面
 		else
 		# 创建一个错误消息
