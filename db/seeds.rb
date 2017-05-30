1e654fa922c1ccf6668370f000a5a1a557ad4640
houchuanhao <houchuanhao@imudges.com>
2017-05-29 22:43:21 -0700

    将管理员设置为admin,实现了管理员删除权限(在_user.html.erb中删了一段代码)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(
-	name: "Example User",
-	email: "example@railstutorial.org",
-	password:"foobar",
-	password_confirmation: "foobar",
+	name: "admin",
+	email: "admin@admin.admin",
+	password:"admin",
+	password_confirmation: "admin",
 	admin: true)
 99.times do |n|
 	name = Faker::Name.name
