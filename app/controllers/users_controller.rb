994c83d1d220bc3aade555cd200a72be7b0b99af
houchuanhao <houchuanhao@imudges.com>
2017-05-29 22:46:38 -0700

    10.5完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,only: [:edit, :update]
+  before_action :admin_user,only: :destroy
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -8,6 +11,12 @@ class UsersController < ApplicationController
   def show
   	@user=User.find(params[:id])
     #@microposts = @user.microposts.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
   def create
   	@user = User.new(user_params)
@@ -48,4 +57,11 @@ private
   @user = User.find(params[:id])
   redirect_to(root_url) unless current_user?(@user)
   end
+
+
+  # 确保是管理员
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
+
